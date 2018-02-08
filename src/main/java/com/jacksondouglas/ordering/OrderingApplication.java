a2a622e470996c1616993013e884db5a3b03c08a
Jackson Douglas <jacksondouglasti@gmail.com>
2018-02-08 19:37:21 -0200

    Changed configuration to application.yml folder.

--- src/main/java/com/jacksondouglas/ordering/OrderingApplication.java
+++ src/main/java/com/jacksondouglas/ordering/OrderingApplication.java
@@ -2,11 +2,19 @@ package com.jacksondouglas.ordering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class OrderingApplication {
 
+    /**
+     * Mode all application*.yml to /config/ordering/ folder.
+     * It's to avoid committing the configuration files with real credentials.
+     * @param args
+     */
     public static void main(String[] args) {
-        SpringApplication.run(OrderingApplication.class, args);
+        new SpringApplicationBuilder(OrderingApplication.class).properties("spring.config.location:/config/ordering/").build().run(args);
     }
 }
